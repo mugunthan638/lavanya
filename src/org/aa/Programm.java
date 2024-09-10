fe6525ba14131dfa2413dbbc925d2f46a3136193
mugunthan638 <mugunthanswetha@gmail.com>
2024-09-10 13:22:51 +0530

    Project

---
+++ src/org/aa/Programm.java
@@ -0,0 +1,17 @@
+package org.aa;
+
+public class Programm {
+	
+	public static void main(String[] args) {
+		
+		String s = "Mugunthan";
+		
+		char c = s.charAt(2);
+		System.out.println(c);
+		
+		
+		
+		
+	}
+
+}
